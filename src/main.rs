a9aa509468f49ac3f5c514ae37f8d95c5bc5f492
szymky <mikaroebers@proton.me>
2026-05-20 18:46:53 +0200

    window stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn conf() -> Conf {
+    Conf {
+        window_title: String::from("Chess"),
+        window_resizable: false,
+        window_width: 600,
+        window_height: 600,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        next_frame().await
+    }
 }

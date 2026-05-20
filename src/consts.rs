cd3f7bfe678c60b2089f1fc4119cf452d5aba75c
szymky <mikaroebers@proton.me>
2026-05-20 20:15:20 +0200

    board

---
+++ src/consts.rs
@@ -0,0 +1,8 @@
+use macroquad::color::Color;
+
+pub const DARK_SQUARE: Color = Color::new(0.7, 0.49, 0.38, 1.0);
+pub const LIGHT_SQUARE: Color = Color::new(0.94, 0.85, 0.7, 1.0);
+// const LIGHT_HOVER: Color = Color::new(0.94, 0.85, 0.7, 0.6);
+// const DARK_HOVER: Color = Color::new(0.7, 0.49, 0.38, 0.6);
+pub const BOARD_WIDTH: usize = 8;
+pub const BOARD_HEIGHT: usize = 8;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use macroquad::prelude::*;
 
+mod consts;
+
+use consts::*;
+
 fn conf() -> Conf {
     Conf {
         window_title: String::from("Chess"),
@@ -12,8 +16,28 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
+    let size = screen_width() / BOARD_WIDTH as f32;
+
     loop {
         clear_background(BLACK);
+
+        // let (mx, my) = mouse_position();
+
+        for i in 0..BOARD_HEIGHT * BOARD_WIDTH {
+            let x = (i % BOARD_WIDTH) as f32;
+            let y = (i / BOARD_HEIGHT) as f32;
+
+            let px = x * size;
+            let py = y * size;
+
+            //let is_hovering = mx < px + size && mx > px && my < py + size && my > py;
+
+            let is_light = (x + y) % 2.0 == 0.0;
+
+            let color = if is_light { LIGHT_SQUARE } else { DARK_SQUARE };
+
+            draw_rectangle(px, py, size, size, color);
+        }
 
         next_frame().await
     }
